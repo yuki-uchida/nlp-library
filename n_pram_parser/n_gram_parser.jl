b7a8710afa2ee8644ee543ebfc94bd99866c9722
yuki-uchida <rantarn0326@gmail.com>
2019-11-09 23:39:57 +0900

    make n_gram_parser library

---
+++ n_pram_parser/n_gram_parser.jl
@@ -0,0 +1,33 @@
+
+module n_gram_parser
+    export make_n_gram
+        function make_n_gram(text::String, parse = "char"::String, n = 2::Int)
+    """ charとwordはオプションで指定する"""
+    if parse == "char"
+        split_list = split(text, r"", keepempty = false)
+        filter!(e->e ≠ " ", split_list)
+    elseif parse == "word"
+        split_list = split(text, " ", keepempty = false)
+    else
+        throw("set char/word parse")
+        return
+    end
+    """ 先に行列の宣言をする """
+    n_gram_list = []
+    """ for文を回して、n-gram_unitを作る処理を繰り返す """
+    """ n-gramはn個のの単語を後ろから取るようにする。なので(n-1)番目までで良い """
+    for i = 1:length(split_list) - (n - 1)
+        """ n-gramの単位行列を作るための宣言をする """
+        """ n語取り出して入れ込む """
+        n_gram_unit_list = [String(split_list[i + j - 1]) for j in 1:n]
+        """ 先に宣言した行列に値を入れ込む """
+        n_gram_list = append!(n_gram_list, [n_gram_unit_list])
+    end
+    """ returnでn-gramの行列を返す """
+    return n_gram_list
+end
+end
+
+
+using .n_gram_parser
+println(n_gram_parser.make_n_gram("I am a NLper", "char", 3))
---
+++ n_pram_parser/n_gram_test.jl
@@ -0,0 +1,11 @@
+include("n_gram_parser.jl")
+# using n_gram_parser
+using Test
+@test n_gram_parser.make_n_gram("I am a NLPer", "word", 2) == Any[["I","am"],["am","a"],["a","NLPer"]]
+
+@test n_gram_parser.make_n_gram("I am a NLPer", "word", 3) == Any[["I","am","a"],["am","a","NLPer"]]
+
+@test n_gram_parser.make_n_gram("I am a NLPer", "char", 2) == Any[["I","a"],["a","m"],["m","a"],["a","N"],["N","L"],["L","P"],["P","e"],["e","r"]]
+
+@test n_gram_parser.make_n_gram("I am a NLPer", "char", 3) == Any[["I","a","m"],["a","m","a"],["m","a","N"],["a","N","L"],["N","L","P"],["L","P","e"],["P","e","r"]]
+
